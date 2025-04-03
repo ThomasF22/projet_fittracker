e11f066181e5b0f344e2996e290f04a0406318fa
roshan.nicolai <roshannicolai@outlook.fr>
2025-04-04 00:12:25 +0200

    test unitaire classe exercice

---
+++ src/test/java/com/fittracker/model/ExerciceTest.java
@@ -0,0 +1,27 @@
+package com.fittracker.model;
+
+import com.fittracker.model.Exercise;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class ExerciseTest {
+
+    @Test
+    void testExerciseCreation() {
+        Exercise exercise = new Exercise(1, "Push-ups");
+
+        assertNotNull(exercise);
+        assertEquals(1, exercise.getId());
+        assertEquals("Push-ups", exercise.getName());
+    }
+
+    @Test
+    void testToString() {
+        Exercise exercise = new Exercise(2, "Squats");
+
+        assertEquals("Squats", exercise.toString());
+    }
+}
